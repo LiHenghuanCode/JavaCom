965d0778e7a5c22267b4c7ef3255316d27e996f8
Henghuan Li <u8063442@anu.edu.au>
2025-07-04 19:55:41 +1000

    Version1

---
+++ src/main/java/com/lhh/nc/util/MailClient.java
@@ -0,0 +1,37 @@
+package com.lhh.nc.util;
+
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailClient {
+    private static final Logger logger = LoggerFactory.getLogger(MailClient.class);
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void sendMail(String to, String subject, String text) {
+        try{
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+            mailSender.send(mimeMessage);
+        } catch (Exception e) {
+            logger.error("Mail Sender Error",e);
+        }
+    }
+
+}
